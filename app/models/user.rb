c84b88ecebc0dc2a0192a8a94eb3891290be6739
Jeff Moore <jmas1_2000@hotmail.com>
2017-07-14 21:09:14 +0000

    Formatted Facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,26 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :omniauthable
-         
+=begin        
+  def self.find_or_create_from_auth_hash(auth_hash)
+    user = where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create
+    user.update(
+      name: auth_hash.info.name,
+      image: auth_hash.info.image,
+      token: auth_hash.credentials.token,
+      secret: auth_hash.credentials.secret
+      )
+      user
+end
+
+  end
+=end
+=begin       
   has_many :identities
 
   def twitter
     identities.where( :provider => "twitter" ).first
   end
-
   def twitter_client
     @twitter_client ||= Twitter.client( access_token: twitter.accesstoken )
   end
@@ -41,6 +54,7 @@ class User < ActiveRecord::Base
     end
     @google_oauth2_client
   end
+=end
   def avatar_url(size)
     gravatar_id = Digest::MD5::hexdigest(self.email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
